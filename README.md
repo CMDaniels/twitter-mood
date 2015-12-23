2aed4c432a4e437306ddd112bc1ef46e77d13902
Cody Daniels <codydaniels@gmail.com>
2015-12-23 12:52:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitter Mood
+## Analyzing Twitter's pulse
---
+++ app.js
@@ -0,0 +1,7 @@
+var Twitter = require('twitter');
+var five = require('johnny-five');
+var board = new five.Board();
+
+board.on('ready', function() {
+
+});
